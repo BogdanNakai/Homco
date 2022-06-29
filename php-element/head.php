a4e4ce1a7accd558de0e2fdcd6e36f98c6067bde
BogdanNakai <108429562+BogdanNakai@users.noreply.github.com>
2022-06-29 23:22:59 +0300

    Add files via upload

---
+++ php-element/head.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="keywords" content="Lorem ipsum dolor sit amet.">
+	<meta name="description" content="Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/normalize.css">
+	<title>Homco</title>
---
+++ php-element/header.php
@@ -0,0 +1,31 @@
+<header class="header">
+			<div class="container-1 header__container-1">
+				<div class="header__logo logo">
+					<a href="Home.php" class="logo__link">
+						<img src="img/Logo Homco 1.png" alt="Homco" class="logo__img">
+					</a>
+				</div>
+				<nav class="header__nav nav">
+					<ul class="nav__menu menu">
+						<li class="menu__item"><a href="#" class="menu__link">Home</a></li>
+						<li class="menu__item"><a href="#" class="menu__link">About Us</a></li>
+						<li class="menu__item"><a href="#" class="menu__link">Our Services</a></li>
+						<li class="menu__item"><a href="#" class="menu__link menu__link_submenu">Our Projects</a></li>
+						<li class="menu__item"><a href="#" class="menu__link">Portfolio</a></li>
+						<li class="menu__item"><a href="#"
+								class="menu__link menu__link_submenu menu__link_submenu-6">Pages</a>
+							<ul class="menu__sub-menu sub-menu">
+								<li class="sub-menu__item"><a href="#" class="sub-menu__link">Faq</a></li>
+								<li class="sub-menu__item"><a href="#" class="sub-menu__link">Blog</a></li>
+								<li class="sub-menu__item"><a href="#" class="sub-menu__link">Single Post</a></li>
+							</ul>
+						</li>
+					</ul>
+					<div class="nav__btn-menu btn-menu">
+						<a href="#" class="btn-menu__link">
+							<img src="img/Offcanvas Menu.png" alt="Menu button" class="btn-menu__img">
+						</a>
+					</div>
+				</nav>
+			</div>
+		</header>
